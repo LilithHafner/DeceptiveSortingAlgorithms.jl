0c4603337ba1f5099bd347b8dae3b09d22b06209
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-12-19 14:48:50 -0600

    Add constant_sort2! that turns back time to cover its tracks (and switch to QuickSort to avoid allocations)

--- src/EvilSortingAlgorithms.jl
+++ src/EvilSortingAlgorithms.jl
@@ -1,6 +1,6 @@
 module EvilSortingAlgorithms
 
-export constant_sort!, ftl_sort!
+export constant_sort!, ftl_sort!, constant_sort2!
 
 to_sort = Channel(Inf)
 
@@ -19,7 +19,21 @@ end
 singularity = false
 function ftl_sort!(v::Vector)
     global singularity = true
-    sort!(v)
+    sort!(v, alg=QuickSort)
+end
+
+const TIME_WARP = Ref(UInt64(0))
+function constant_sort2!(v::Vector)
+    t0 = ccall(:jl_hrtime, UInt64, ())
+    sort!(v, alg=QuickSort)
+    t1 = ccall(:jl_hrtime, UInt64, ())
+    TIME_WARP[] += t0-t1
+    v
+end
+function Base.time_ns()
+    res = ccall(:jl_hrtime, UInt64, ()) + TIME_WARP[]
+    TIME_WARP[] = 0
+    res
 end
 
 end
