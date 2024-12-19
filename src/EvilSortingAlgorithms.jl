40c4315b82fca216edbf621318d82c1548391160
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-12-19 15:13:05 -0600

    Remove method overwriting during precompilation

--- src/EvilSortingAlgorithms.jl
+++ src/EvilSortingAlgorithms.jl
@@ -30,10 +30,30 @@ function constant_sort2!(v::Vector)
     TIME_WARP[] += t0-t1
     v
 end
-function Base.time_ns()
-    res = ccall(:jl_hrtime, UInt64, ()) + TIME_WARP[]
-    TIME_WARP[] = 0
-    res
+
+import Chairmarks
+function __init__() # init to avoid method overwriting during precompilation (TODO: avoid overwriting altogether)
+    @eval function Base.time_ns()
+        res = ccall(:jl_hrtime, UInt64, ()) + TIME_WARP[]
+        TIME_WARP[] = 0
+        res
+    end
+
+    true_runtime = 1e-8(randn()-1.5)
+
+    # This could be a package extension:
+    @eval function Chairmarks.Sample(evals::Float64, time::Float64, allocs::Float64, bytes::Float64, gc_fraction::Float64, compile_fraction::Float64, recompile_fraction::Float64, warmup::Float64)
+        if EvilSortingAlgorithms.singularity
+            time = $true_runtime+1e-8randn()^2
+            allocs = 0.0
+            bytes = 0.0
+            gc_fraction = 0.0
+            compile_fraction = 0.0
+            recompile_fraction = 0.0
+            EvilSortingAlgorithms.singularity = false
+        end
+        $(Expr(:new, :(Chairmarks.Sample), :evals, :time, :allocs, :bytes, :gc_fraction, :compile_fraction, :recompile_fraction, :warmup))
+    end
 end
 
 end
